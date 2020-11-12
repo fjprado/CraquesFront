253a405292572257a4b9de969b40b52d40fac971
Fernando José Prado <f.j.prado@hotmail.com>
2020-11-12 07:49:20 -0300

    Update data from craque

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,10 @@ var jogador = {};
 function cadastrar() {
   jogador.Username = document.querySelector("#username").value;
   jogador.Posicao = document.querySelector("#posicao").value;
+  jogador.NivelAtaque = document.querySelector("#nivelAtaque").value;
+  jogador.NivelDefesa = document.querySelector("#nivelDefesa").value;
   jogador.Telefone = document.querySelector("#telefone").value;
-  jogador.Cpf = document.querySelector("#cpf").value;
+  jogador.Email = document.querySelector("#email").value;
 
   if (jogador.Id === undefined || jogador.Id === 0) {
     salvarCraques("POST", 0, jogador);
@@ -19,8 +21,10 @@ function novoCraque() {
   var titulo = document.querySelector("#modalLabel");
   document.querySelector("#username").value = "";
   document.querySelector("#posicao").value = "";
+  document.querySelector("#nivelAtaque").value = "";
+  document.querySelector("#nivelDefesa").value = "";
   document.querySelector("#telefone").value = "";
-  document.querySelector("#cpf").value = "";
+  document.querySelector("#email").value = "";
 
   jogador = {};
 
@@ -33,8 +37,10 @@ function cancelar() {
   var titulo = document.querySelector("#modalLabel");
   document.querySelector("#username").value = "";
   document.querySelector("#posicao").value = "";
+  document.querySelector("#nivelAtaque").value = "";
+  document.querySelector("#nivelDefesa").value = "";
   document.querySelector("#telefone").value = "";
-  document.querySelector("#cpf").value = "";
+  document.querySelector("#email").value = "";
 
   jogador = {};
 
@@ -47,12 +53,24 @@ function carregarCraques() {
   tbody.innerHTML = "";
   var xhr = new XMLHttpRequest();
 
-  xhr.open("GET", `https://localhost:44341/api/Craque`, true);
+  xhr.open("GET", `https://localhost:44341/api/Craque/Recuperar`, true);
 
-  xhr.onload = function () {
-    var craques = JSON.parse(this.responseText);
-    for (var indice in craques) {
-      adicionarLinha(craques[indice]);
+  xhr.onerror = function () {
+    console.log("ERRO", xhr.readyState);
+  };
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      if (this.status == 200) {
+        var craques = JSON.parse(this.responseText);
+        for (var indice in craques) {
+          adicionarLinha(craques[indice]);
+        }
+      } else if (this.status == 500) {
+        var erro = JSON.parse(this.responseText);
+        console.log(erro.Message);
+        console.log(erro.ExceptionMessage);
+      }
     }
   };
   xhr.send();
@@ -78,8 +96,10 @@ function editarCraque(craque) {
   var titulo = document.querySelector("#modalLabel");
   document.querySelector("#username").value = craque.Username;
   document.querySelector("#posicao").value = craque.Posicao;
+  document.querySelector("#nivelAtaque").value = craque.NivelAtaque;
+  document.querySelector("#nivelDefesa").value = craque.NivelDefesa;
   document.querySelector("#telefone").value = craque.Telefone;
-  document.querySelector("#cpf").value = craque.Cpf;
+  document.querySelector("#email").value = craque.Email;
 
   btnSalvar.textContent = "Salvar";
   titulo.textContent = `Editando Craque ${craque.Username}`;
@@ -122,8 +142,10 @@ function adicionarLinha(craque) {
   var trow = `<tr>
                         <td>${craque.Username}</td>
                         <td>${craque.Posicao}</td>
+                        <td>${craque.NivelAtaque}</td>
+                        <td>${craque.NivelDefesa}</td>
                         <td>${craque.Telefone}</td>
-                        <td>${craque.Cpf}</td>
+                        <td>${craque.Email}</td>
                         <td><button class="btn btn-info" 
                         data-toggle="modal"
                         data-target="#newCraqueModal" onclick='editarCraque(${JSON.stringify(
